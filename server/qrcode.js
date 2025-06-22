aa5026011d987a99e9e36a758c9c8bbc9b0b5ddc
Hexa <ghstdu@gmail.com>
2025-06-22 12:02:30 +0100

    QrCode + Unique token generation

---
+++ server/qrcode.js
@@ -0,0 +1,33 @@
+const qr = require('qrcode');
+const uuid = require('uuid');
+
+const generateQrUniqueData = () => {
+    
+    const uniqueId = uuid.v4();
+    const qrdata = {
+        id : uniqueId,
+        isUsed: false
+    };
+
+    return JSON.stringify(qrdata);
+}
+
+const generateQrCode = async (data)=>{
+
+    try{
+        const qrUrl = await qr.toDataURL(data);
+        return qrUrl;
+    }catch (error) {
+        console.error("Error generating QR code:", error);
+        throw error;
+    }
+
+}
+
+generateQrCode(generateQrUniqueData())
+.then((qrUrl) => {
+    console.log("QR Code URL:", qrUrl);
+})
+.catch((error) => {
+    console.error("Error:", error);
+});
