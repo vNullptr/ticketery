b1b65046f820cee85cae70fd512f32d52ed6f0ec
Hexa <ghstdu@gmail.com>
2025-06-24 19:04:31 +0100

    Added models for tickets

---
+++ server/models/tickets.js
@@ -0,0 +1,18 @@
+
+export const ticketOptions = [
+    {
+        "name": "VIP",
+        "price": "500.0",
+
+    },
+    {
+        "name": "Front",
+        "price": "200.0",
+
+    },
+    {
+        "name": "Backstage",
+        "price": "600.0",
+
+    }
+]
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import { ticketOptions } from '.././models/tickets.js';
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send('API is running!');
 });
+
+router.get("/tickets", (req, res)=>{
+    res.json(ticketOptions);
+})
 
 export default router;
 
