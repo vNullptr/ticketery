5baf86a5db371d53ff72b33015a62873bb41df3e
Hexa <ghstdu@gmail.com>
2025-07-01 17:49:03 +0100

    changed tickets

--- server/models/tickets.js
+++ server/models/tickets.js
@@ -1,22 +1,13 @@
 
 export const ticketOptions = [
     {
-        "name": "VIP",
-        "price": "500.0",
+        "name": "Backstage",
+        "price": "1050.0",
 
     },
     {
         "name": "Front",
-        "price": "200.0",
+        "price": "500.0",
 
-    },
-    {
-        "name": "Backstage",
-        "price": "800.0",
-
-    },
-    {
-        "name": "Random category",
-        "price": "1000.0",
     }
 ]
